6e9ed84bfa3a04a4e3a38b190121c5b536a0b68a
Usuario <julian19guerra@gmail.com@DESKTOP-FM3NDOL>
2024-12-15 17:43:31 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyLibrary;
+
+Secretary Maria = new Secretary();
+Maria.Name = "Maria";
+
+Manager Pedro = new Manager();
+Pedro.Name = "Pedro";
+
+Developer Miguel = new Developer();
+Miguel.Name = "Miguel";
+
+WriteEmployeeInfo(Maria);
+WriteEmployeeInfo(Pedro);
+WriteEmployeeInfo(Miguel);
+
+void WriteEmployeeInfo(Employee employee)
+{
+    Console.WriteLine($"{employee.Name}: {employee.CalculateMonthlyPayment()}");
+    Console.WriteLine($"Days: {employee.GetPaymentDays()}");
+}
+
+Console.WriteLine($"Días: {Miguel.GetPaymentDays()}");
